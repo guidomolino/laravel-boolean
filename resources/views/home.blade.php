add3358b9724b4f8cc514ff1cca1a77d467e974e
Guido <molido@hotmail.it>
2020-09-21 07:39:53 +0200

    fix 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,20 +6,30 @@
     <h1>LE LUNGHE</h1>
     <div class="pasta-container">
       @foreach ($data as $dato)
-        <img src="{{ $dato->src }}" alt="">
+        @if ($dato->tipo == "lunga")
+          <img src="{{ $dato->src }}" alt="{{ $dato->titolo }}">
+        @endif
       @endforeach
     </div>
   </section>
   <section>
     <h1>LE CORTE</h1>
     <div class="pasta-container">
-
+      @foreach ($data as $dato)
+        @if ($dato->tipo == "corta")
+          <img src="{{ $dato->src }}" alt="{{ $dato->titolo }}">
+        @endif
+      @endforeach
     </div>
   </section>
   <section>
     <h1>LE CORTISSIME</h1>
     <div class="pasta-container">
-
+      @foreach ($data as $dato)
+        @if ($dato->tipo == "cortissima")
+          <img src="{{ $dato->src }}" alt="{{ $dato->titolo }}">
+        @endif
+      @endforeach
     </div>
   </section>
 
