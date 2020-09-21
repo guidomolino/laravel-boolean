fe53f725670c279204953e3cff1e9ead6d6b66d6
Guido <molido@hotmail.it>
2020-09-21 10:20:13 +0200

    cambio del font a roboto

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -3,6 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>laravel</title>
+    {{-- font --}}
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    {{-- css --}}
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
   </head>
   <body>
