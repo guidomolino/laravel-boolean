ed7eddde1fccb33e98eb32d02363744f3acf2109
Guido <molido@hotmail.it>
2020-09-21 11:18:56 +0200

    aggiornamento grafico

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,54 +1,56 @@
-<footer class="container">
-  {{-- info azienda, contatti e locations --}}
-  <div class="info">
-    <img src="{{asset('images/marchio-sito-test.png')}}" alt="">
-    <ul>
-      <li>Ragione sociale: La Molisana S.P.A.</li>
-      <li>Sede legale: Contrada Colle delle Api, 100/A 86100 Campobasso (CB) </li>
-      <li>Pec: <br> lamolisana@pec.it </li>
-      <li>Tel: +39 0874 4981</li>
-      <li>Fax: +39 0874 629584 </li>
-      <li>info@lamolisana.it (per segnalazioni degli utenti</li>
-      <li>commerciale@lamolisana.it</li>
-      <li>export@lamolisana.it</li>
-      <li>numero verde <br> 800818081</li>
-      <li>telefono <br> 3801292455 </li>
-    </ul>
-  </div>
-  {{-- refernces --}}
-  <div class="">
-    <h2>PASTIFICIO</h2>
-    <ul>
-      <li>Il Pasticio</li>
-      <li>Grano decorticato a pietra</li>
-      <li>Il Molise c'è</li>
-      <li>Filiera Integrata</li>
-      <li>100 anni di pasta</li>
-      <li>Sartoria della pasta</li>
-      <li>Spaghetto Quadrato</li>
-      <li>Le Persone</li>
-    </ul>
-  </div>
-  <div class="">
-    <h2>PRODOTTI</h2>
-    <ul>
-      <li>Le Classiche</li>
-      <li>Le Integrali</li>
-      <li>Le Speciali</li>
-      <li>Le Biologiche</li>
-      <li>Le Gluten Free</li>
-      <li>Le Semole</li>
-      <li>Le Extra di Lusso</li>
-    </ul>
-  </div>
-  <div class="">
-    <h2>COLLEZIONE DA CHEF</h2>
-    <ul>
-      <li>Collezione da Chef</li>
-      <li>Grandi Cucine</li>
-      <li>Biologiche</li>
-      <li>Quadrate</li>
-    </ul>
-  </div>
+<footer>
+  <div class="container">
+    {{-- info azienda, contatti e locations --}}
+    <div class="info">
+      <img src="{{asset('images/marchio-sito-test.png')}}" alt="">
+      <ul>
+        <li>Ragione sociale: La Molisana S.P.A.</li>
+        <li>Sede legale: Contrada Colle delle Api, 100/A 86100 Campobasso (CB) </li>
+        <li>Pec: <br> lamolisana@pec.it </li>
+        <li>Tel: +39 0874 4981</li>
+        <li>Fax: +39 0874 629584 </li>
+        <li>info@lamolisana.it (per segnalazioni degli utenti</li>
+        <li>commerciale@lamolisana.it</li>
+        <li>export@lamolisana.it</li>
+        <li>numero verde <br> 800818081</li>
+        <li>telefono <br> 3801292455 </li>
+      </ul>
+    </div>
+    {{-- refernces --}}
+    <div class="filiates">
+      <h2>PASTIFICIO</h2>
+      <ul>
+        <li>Il Pasticio</li>
+        <li>Grano decorticato a pietra</li>
+        <li>Il Molise c'è</li>
+        <li>Filiera Integrata</li>
+        <li>100 anni di pasta</li>
+        <li>Sartoria della pasta</li>
+        <li>Spaghetto Quadrato</li>
+        <li>Le Persone</li>
+      </ul>
+    </div>
+    <div class="filiates">
+      <h2>PRODOTTI</h2>
+      <ul>
+        <li>Le Classiche</li>
+        <li>Le Integrali</li>
+        <li>Le Speciali</li>
+        <li>Le Biologiche</li>
+        <li>Le Gluten Free</li>
+        <li>Le Semole</li>
+        <li>Le Extra di Lusso</li>
+      </ul>
+    </div>
+    <div class="filiates">
+      <h2>COLLEZIONE DA CHEF</h2>
+      <ul>
+        <li>Collezione da Chef</li>
+        <li>Grandi Cucine</li>
+        <li>Biologiche</li>
+        <li>Quadrate</li>
+      </ul>
+    </div>
 
+  </div>
 </footer>
